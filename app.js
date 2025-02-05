af15d18e1e1e64348111f604f15be8e6f63979c3
Admin Cortez <sobekmc1312@gmail.com>
2025-02-05 16:30:21 -0500

    arreglar que se abran en otra pagina

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const nombreElemento = document.querySelector(".nombre");
+    const descripcionElemento = document.querySelector(".descripcion");
+
+    nombreElemento.textContent="Rodrigo Cortez Gamio";
+    descripcionElemento.textContent="Por cortesia de su amigo y vecino, el hombre araña."
+})
